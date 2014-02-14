7d032c47eee9916d4414b998c851828557985762
brsoff <brsoff@gmail.com>
2014-02-14 12:30:02 -0500

    got basic functionality working

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -9,73 +9,78 @@ var site = {
         $web_development_inner = $("#web_development_inner");
         $web_design_inner = $("#web_design_inner");
         $about_me_inner = $("#about_me_inner");
-        $close_me = $(".close-me");
+        // $close_me = $(".close-me");
+
+        site.listeners();
     },
 
     listeners: function () {
 
-        $web_development.on("click", function () {
+        $web_development.off("click.webdev").on("click.webdev", function () {
             console.log("web dev clicked")
             var me = $(this)
-            $web_design.hide()
-            $about_me.hide()
             animateDiv(me);
 
         });
 
-        $web_design.on("click", function () {
+        $web_design.off("click.webdesign").on("click.webdesign", function () {
             console.log("web design clicked")
             var me = $(this)
-            $web_development.hide();
-            $about_me.hide();
             animateDiv(me);
 
         });
 
-        $about_me.on("click", function () {
+        $about_me.off("click.aboutme").on("click.aboutme", function () {
             console.log("about me clicked")
             var me = $(this)
-            $web_development.hide();
-            $web_design.hide();
             animateDiv(me);
         });
 
         function animateDiv (active_div) {
-            $main.animate({"margin-top":"20px"});
+            active_div.css("pointer-events", "none");
             active_div.animate({ "width": "100%" });
             var $inner_div = active_div.find('.main-block-inner');
             var $h2 = $inner_div.find("h3");
-            $inner_div.animate({ "height": "500px", "padding-top": "20px" });
+            $inner_div.animate({ "height": "500px" });
+            $inner_div.css("padding", "20px")
 
             $content_div = $inner_div.find('.content');
             $content_div.append("hello");
 
             $h2.animate({"font-size":"32px"});
-            $close_me.fadeIn();
-            site.closelistener(active_div);
+
+            var $close_div = $("<div>").addClass("close-me").text("close")
+
+            active_div.before($close_div);
+
+            $close_div.fadeIn();
+
+            site.closelistener(active_div, $close_div);
         }
 
     },
 
-    closelistener: function (open_div) {
-        $close_me.off("click").on("click", function () {
+    closelistener: function (open_div, close_div) {
 
-            $main.animate({"margin-top":"100px"});
-            $(this).hide();
-            $web_design.show();
-            $about_me.show();
-            $web_development.show();
-            open_div.css({ "width": "33%" });
+        close_div.off("click").on("click", function () {
+
+            open_div.css({ "width": "200px" });
 
             $content_div = open_div.find('.content');
             $content_div.empty();
 
             var $inner_div = open_div.find('.main-block-inner');
-            $inner_div.css({ "height": "120px", "padding-top": "90px"});
+
+            $inner_div.css({ "height": "auto", "padding": "10px 5px"});
             var $h2 = $inner_div.find("h3");
             $h2.css({"font-size":"16px"});
 
+            open_div.css("pointer-events", "all");
+
+            $(this).remove()
+
         });
+
 
     }
 
@@ -89,9 +94,7 @@ $(function () {
         $about_me,
         $web_development_inner,
         $web_design_inner,
-        $about_me_inner,
-        $close_me;
+        $about_me_inner;
 
     site.initialize();
-    site.listeners();
 })
