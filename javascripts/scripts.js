9d043dd4c49dc95bd192f7ce6b90cadc4a8ef2bf
brsoff <brsoff@gmail.com>
2014-02-15 00:13:50 -0500

    added yellow bg

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -9,7 +9,11 @@ var site = {
         $web_development_inner = $("#web_development_inner");
         $web_design_inner = $("#web_design_inner");
         $about_me_inner = $("#about_me_inner");
+
+        $intro = $("<div>").addClass("intro").html("<p>Hello, my name is Brendan and I'm a web developer in New York. </p><p>Check out the menu to the left to see some of the neat stuff I've completed, or visit my <a href=http://www.github.com/brsoff>github.</p><p></p>")
         // $close_me = $(".close-me");
+
+        $main.prepend($intro);
 
         site.listeners();
     },
@@ -37,21 +41,24 @@ var site = {
         });
 
         function animateDiv (active_div) {
+            $intro.fadeOut("fast");
             active_div.css("pointer-events", "none");
-            active_div.animate({ "width": "100%" });
+            active_div.animate({ "width": "100%" }, "fast");
             var $inner_div = active_div.find('.main-block-inner');
             var $h2 = $inner_div.find("h3");
             $inner_div.animate({ "height": "500px" });
-            $inner_div.css("padding", "20px")
+            $inner_div.addClass("feature")
 
             $content_div = $inner_div.find('.content');
             $content_div.append("hello");
+
+            
 
             $h2.animate({"font-size":"32px"});
 
             var $close_div = $("<div>").addClass("close-me").text("close")
 
-            active_div.before($close_div);
+            active_div.parent().prepend($close_div);
 
             $close_div.fadeIn();
 
@@ -70,12 +77,20 @@ var site = {
             $content_div.empty();
 
             var $inner_div = open_div.find('.main-block-inner');
+            $inner_div.removeClass("feature");
 
-            $inner_div.css({ "height": "auto", "padding": "10px 5px"});
+            $inner_div.css({ "height": "auto" });
+
             var $h2 = $inner_div.find("h3");
             $h2.css({"font-size":"16px"});
 
             open_div.css("pointer-events", "all");
+
+            if ( ($web_development.css("width") != "200px") || ($web_design.css("width") != "200px") || ($about_me.css("width") != "200px") ) {
+
+            }else{
+                $intro.fadeIn();
+            }
 
             $(this).remove()
 
