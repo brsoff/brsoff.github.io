e7638730e2285d094bbcd860e4281a527d886162
brsoff <brsoff@gmail.com>
2014-02-15 18:30:56 -0500

    added portfolio and resume

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -58,15 +58,7 @@ var site = {
             active_div.parent().prepend($close_div);
             $close_div.fadeIn();
 
-            if (active_div.attr('id') === "portfolio") {
-                $content_div.append(site.content.portfolio());
-            }else if (active_div.attr('id') === "resume") {
- 
-            }else if (active_div.attr('id') === "about_me") {
- 
-            }else{
-                console.log("(._. )")
-            }
+            active_div.find(".content").fadeIn();
 
             site.closeListener(active_div, $close_div);
     },
@@ -76,7 +68,7 @@ var site = {
         close_div.off("click").on("click", function () {
             open_div.css({ "width": "200px" });
             $content_div = open_div.find('.content');
-            $content_div.empty();
+            $content_div.hide();
             var $inner_div = open_div.find('.main-block-inner');
             $inner_div.removeClass("feature");
             $inner_div.css({ "height": "auto" });
@@ -108,33 +100,10 @@ var site = {
     content: {
 
       intro: function () {
-        var intro =  $("<div>").addClass("intro").html("<p>Hey! My name is Brendan and I'm a web developer in New York.</p><p>I work primarily with Ruby on Rails, Javascript and jQuery, and have lots of experience with Wordpress, Magento, Shopify and many more CMS's and frameworks.</p><p>Check out the menu to the left to see some of the neat stuff I've completed or visit my <a href=http://www.github.com/brsoff>github</a>.</p><p>Also, don't hesitate to send me an email at brsoff at gmail.</p>");
+        var intro =  $("<div>").addClass("intro").html("<p>Hello! My name is Brendan and I'm a web developer and designer in New York.</p><p>I work primarily with Ruby on Rails, Javascript and jQuery, and have lots of experience with Wordpress, Magento, Shopify and many more CMS's and frameworks.</p><p>Check out the menu to the left to see some of the neat stuff I've completed or visit my <a href=http://www.github.com/brsoff>github</a>.</p><p>Also, don't hesitate to send me an email at brsoff at gmail.</p>");
 
         return intro;
-      },
-
-      portfolio: function () {
-
-        var output = "<h2>(Web Development)</h2>";
-        output += "<div class=tri><div class=tri-inner>example</div></div>";
-        output += "<div class=tri><div class=tri-inner>example</div></div>";
-        output += "<div class=tri><div class=tri-inner>example</div></div>";
-        output += "<div class=tri><div class=tri-inner>example</div></div>";
-        output += "<div class=tri><div class=tri-inner>example</div></div>";
-        output += "<div class=tri><div class=tri-inner>example</div></div>";
-
-        output += "<h2>(Web Design)</h2>";
-        output += "<div class=tri><div class=tri-inner>example</div></div>";
-        output += "<div class=tri><div class=tri-inner>example</div></div>";
-        output += "<div class=tri><div class=tri-inner>example</div></div>";
-        output += "<div class=tri><div class=tri-inner>example</div></div>";
-        output += "<div class=tri><div class=tri-inner>example</div></div>";
-        output += "<div class=tri><div class=tri-inner>example</div></div>";
-
-        return output;
-
       }
-
 
     }
 
