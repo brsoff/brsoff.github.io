c66dd166083c60225adc996aafec583a56659946
brsoff <brsoff@gmail.com>
2014-02-15 02:06:36 -0500

    added some content, renamed sections

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,54 +1,44 @@
 var site = {
 
     initialize: function () {
-        //all the divs
         $body = $("body");
         $main = $("#main");
-        $web_development = $("#web_development");
-        $web_design = $("#web_design");
+        $portfolio = $("#portfolio");
+        $resume = $("#resume");
         $about_me = $("#about_me");
-        $web_development_inner = $("#web_development_inner");
-        $web_design_inner = $("#web_design_inner");
-        $about_me_inner = $("#about_me_inner");
-
-        $intro = $("<div>").addClass("intro").html("<p>Hello, my name is Brendan and I'm a web developer in New York. </p><p>Check out the menu to the left to see some of the neat stuff I've completed, or visit my <a href=http://www.github.com/brsoff>github.</p><p></p>")
+        $intro = site.content.intro();
 
         $main.prepend($intro);
-        site.listeners.webdevlistener();
-        site.listeners.webdesignlistener();
-        site.listeners.aboutmelistener();
+
+        site.listeners.portfolioListener();
+        site.listeners.resumeListener();
+        site.listeners.aboutMeListener();
     },
 
     listeners: {
 
-        webdevlistener: function () {
-
-            $body.one("click", "#web_development", function () {
-                console.log("web dev clicked")
+        portfolioListener: function () {
+            $body.one("click", "#portfolio", function () {
+                console.log("portfolio clicked")
                 var me = $(this)
                 site.animateDiv(me);
             });
-
         },
 
-        webdesignlistener: function () {
-
-            $body.one("click", "#web_design", function () {
-                console.log("web design clicked")
+        resumeListener: function () {
+            $body.one("click", "#resume", function () {
+                console.log("resume clicked")
                 var me = $(this)
                 site.animateDiv(me);
             });
-
         },
 
-        aboutmelistener: function () {
-
+        aboutMeListener: function () {
             $body.one("click", "#about_me", function () {
                 console.log("about me clicked")
                 var me = $(this)
                 site.animateDiv(me);
             });
-
         }
 
     },
@@ -59,22 +49,32 @@ var site = {
             active_div.animate({ "width": "100%" }, "fast");
             var $inner_div = active_div.find('.main-block-inner');
             var $h2 = $inner_div.find("h3");
-            $inner_div.animate({ "height": "500px" });
+            $inner_div.animate({ "height": "auto" });
             $inner_div.addClass("feature")
             $content_div = $inner_div.find('.content');
-            $content_div.append("<a href=http://google.com>hello</a>");
+            
             $h2.animate({"font-size":"32px"});
             var $close_div = $("<div>").addClass("close-me").text("close")
             active_div.parent().prepend($close_div);
             $close_div.fadeIn();
-            site.closelistener(active_div, $close_div);
+
+            if (active_div.attr('id') === "portfolio") {
+                $content_div.append(site.content.portfolio());
+            }else if (active_div.attr('id') === "resume") {
+ 
+            }else if (active_div.attr('id') === "about_me") {
+ 
+            }else{
+                console.log("(._. )")
+            }
+
+            site.closeListener(active_div, $close_div);
     },
 
-    closelistener: function (open_div, close_div) {
+    closeListener: function (open_div, close_div) {
 
         close_div.off("click").on("click", function () {
             open_div.css({ "width": "200px" });
-
             $content_div = open_div.find('.content');
             $content_div.empty();
             var $inner_div = open_div.find('.main-block-inner');
@@ -83,27 +83,57 @@ var site = {
             var $h2 = $inner_div.find("h3");
             $h2.css({"font-size":"16px"});
 
-            if ( ($web_development.css("width") != "200px") || ($web_design.css("width") != "200px") || ($about_me.css("width") != "200px") ) {
-
+            if ( ($portfolio.css("width") != "200px") || ($resume.css("width") != "200px") || ($about_me.css("width") != "200px") ) {
             }else{
                 $intro.fadeIn();
             }
 
-            // console.log(open_div.attr('id'))
-            // console.log($web_development.attr('id'))
+            $(this).remove();
 
-            $(this).remove()
-
-            if (open_div.attr('id') === $web_development.attr('id')) {
-                // console.log("true")
-                site.listeners.webdevlistener();
-            }else if (open_div.attr('id') === $web_design.attr('id')) {
-                site.listeners.webdesignlistener();
+            if (open_div.attr('id') === "portfolio") {
+                site.listeners.portfolioListener();
+            }else if (open_div.attr('id') === "resume") {
+                site.listeners.resumeListener();
+            }else if (open_div.attr('id') === "about_me") {
+                site.listeners.aboutMeListener();
             }else{
-                site.listeners.aboutmelistener();
+                console.log("(._. )")
             }
 
         });
+
+    },
+
+
+    content: {
+
+      intro: function () {
+        var intro =  $("<div>").addClass("intro").html("<p>Hey! My name is Brendan and I'm a web developer in New York.</p><p>I work primarily with Ruby on Rails, Javascript and jQuery, and have lots of experience with Wordpress, Magento, Shopify and many more CMS's and frameworks.</p><p>Check out the menu to the left to see some of the neat stuff I've completed or visit my <a href=http://www.github.com/brsoff>github</a>.</p><p>Also, don't hesitate to send me an email at brsoff at gmail.</p>");
+
+        return intro;
+      },
+
+      portfolio: function () {
+
+        var output = "<h2>(Web Development)</h2>";
+        output += "<div class=tri><div class=tri-inner>example</div></div>";
+        output += "<div class=tri><div class=tri-inner>example</div></div>";
+        output += "<div class=tri><div class=tri-inner>example</div></div>";
+        output += "<div class=tri><div class=tri-inner>example</div></div>";
+        output += "<div class=tri><div class=tri-inner>example</div></div>";
+        output += "<div class=tri><div class=tri-inner>example</div></div>";
+
+        output += "<h2>(Web Design)</h2>";
+        output += "<div class=tri><div class=tri-inner>example</div></div>";
+        output += "<div class=tri><div class=tri-inner>example</div></div>";
+        output += "<div class=tri><div class=tri-inner>example</div></div>";
+        output += "<div class=tri><div class=tri-inner>example</div></div>";
+        output += "<div class=tri><div class=tri-inner>example</div></div>";
+        output += "<div class=tri><div class=tri-inner>example</div></div>";
+
+        return output;
+
+      }
 
 
     }
@@ -112,13 +142,12 @@ var site = {
 
 
 $(function () {
-
-    var $web_development,
-        $web_design,
+    var $portfolio,
+        $resume,
         $about_me,
-        $web_development_inner,
-        $web_design_inner,
-        $about_me_inner;
+        $body,
+        $main,
+        $intro;
 
     site.initialize();
 })
