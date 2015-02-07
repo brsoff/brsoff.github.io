a16a5387f68773a86b462df2c68333f6fae2aae7
brsoff <brsoff@gmail.com>
2015-02-07 10:09:57 -0500

    Switch to regular js

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,18 +4,43 @@ var site = {
   },
 
   bind: function () {
-    $('[data-behavior="open-section"]').on('click', function () {
-      if (!$('.intro').hasClass('closed')) $('.intro').addClass('closed');
-      $(this).closest('.section-holder').addClass('open-section');
-    });
+    var openSectionButtons = document.querySelectorAll("[data-behavior='open-section']");
+    var closeSectionButtons = document.querySelectorAll("[data-behavior='close-section']");
 
-    $('[data-behavior="close-section"]').on('click', function () {
-      $(this).closest('.section-holder').removeClass('open-section');
-      if ($('.open-section').length < 1) $('.intro').removeClass('closed');
-    });
+    for (var i = 0; i < openSectionButtons.length; i++) {
+      openSectionButtons[i].addEventListener("click", function(event) {
+        var intro = document.getElementById("intro");
+        var sectionHolder = this.parentElement;
+
+        if (!hasClass(intro, "closed")) addClass(intro, "closed");
+        addClass(sectionHolder, "open-section");
+      });
+    }
+
+    for (var i = 0; i < closeSectionButtons.length; i++) {
+      closeSectionButtons[i].addEventListener("click", function(event) {
+        var intro = document.getElementById("intro");
+        var sectionHolder = this.parentElement;
+        var openSections = document.getElementsByClassName("open-section");
+
+        removeClass(sectionHolder, "open-section");
+        if (openSections.length < 1) removeClass(intro, "closed");
+      });
+    }
   }
 }
 
-$(function () {
-  site.initialize();
-})
+site.initialize();
+
+function hasClass(element, classname) {
+   return (element.className.split(' ')).indexOf(classname) > -1;
+}
+
+function addClass(element, classname) {
+ element.className = element.className.replace(' ' + classname, '');
+ element.className = element.className + ' ' + classname;
+}
+
+function removeClass(element, classname) {
+ element.className = element.className.replace(' ' + classname, '');
+}
